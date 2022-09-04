70651efebadc59c631be9dca811927cc64ffd84f
Pablo <pablo.baldez@getmayd.com>
2022-09-04 15:37:22 +0200

    feat: log custom properties in production

--- app/src/main/java/br/com/mob1st/bet/core/logs/ThrowableDebugger.kt
+++ app/src/main/java/br/com/mob1st/bet/core/logs/ThrowableDebugger.kt
@@ -6,11 +6,11 @@ package br.com.mob1st.bet.core.logs
  * In cases you can't make an exception implements the [Debuggable] interface, such as exceptions
  * implemented by frameworks or libraries
  */
-interface ThrowableDebugger {
+interface ThrowableDebugger<T : Throwable> {
 
     /**
      * For the cases some important properties
      */
-    fun propertiesFrom(throwable: Throwable): Map<String, Any>
+    fun propertiesFrom(throwable: T): Map<String, Any>
 
 }
--- app/src/main/java/br/com/mob1st/bet/core/timber/CrashReportingTree.kt
+++ app/src/main/java/br/com/mob1st/bet/core/timber/CrashReportingTree.kt
@@ -1,8 +1,13 @@
 package br.com.mob1st.bet.core.timber
 
 import android.util.Log
+import br.com.mob1st.bet.core.firebase.FirebaseExceptionDebugger
+import br.com.mob1st.bet.core.logs.getPropertiesTree
+import br.com.mob1st.bet.core.utils.toFormat
+import com.google.firebase.crashlytics.CustomKeysAndValues
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
+import java.util.Date
 
 class CrashReportingTree(
     private val crashlytics: FirebaseCrashlytics,
@@ -13,12 +18,37 @@ class CrashReportingTree(
         }
         crashlytics.setCustomKey(LOG_PRIORITY_KEY, priority)
         crashlytics.log(message)
+        setCustomProperties(t)
         if (t != null) {
             crashlytics.recordException(t)
+        }
+    }
+
+    private fun setCustomProperties(throwable: Throwable?) {
+        throwable?.getPropertiesTree(FirebaseExceptionDebugger)?.let { map ->
+            crashlytics.setCustomKeys(map.toCustomKeyValues().build())
         }
     }
 
     companion object {
         private const val LOG_PRIORITY_KEY = "LOG_PRIORITY_KEY"
     }
+}
+
+private fun Map<String, Any>.toCustomKeyValues(
+    builder: CustomKeysAndValues.Builder = CustomKeysAndValues.Builder()
+): CustomKeysAndValues.Builder {
+    entries.forEach { property ->
+        when (val value = property.value) {
+            is String -> builder.putString(property.key, value)
+            is Int -> builder.putInt(property.key, value)
+            is Double -> builder.putDouble(property.key, value)
+            is Long -> builder.putLong(property.key, value)
+            is Float -> builder.putFloat(property.key, value)
+            is Boolean -> builder.putBoolean(property.key, value)
+            is Date -> builder.putString(property.key, value.toFormat())
+            else -> builder.putString(property.key, property.value.toString())
+        }
+    }
+    return builder
 }
