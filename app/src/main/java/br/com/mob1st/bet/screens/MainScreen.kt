2da2dfed4664e4d61f83ec717c73eb6c20e67943
Pablo <pablo.baldez@getmayd.com>
2022-09-05 21:05:20 +0200

    feat: remove classes references that will not be commited

--- app/src/main/java/br/com/mob1st/bet/screens/MainScreen.kt
+++ app/src/main/java/br/com/mob1st/bet/screens/MainScreen.kt
@@ -1,26 +1,21 @@
 package br.com.mob1st.bet.screens
 
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.runtime.collectAsState
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import br.com.mob1st.bet.features.competitions.presentation.CompetitionListPage
-import br.com.mob1st.bet.features.competitions.presentation.GetCompetitionEvent
-import br.com.mob1st.bet.features.competitions.presentation.GetCompetitionViewModel
-import org.koin.androidx.compose.koinViewModel
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,15 +32,9 @@ fun MainScreen() {
 
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
-    val getCompetitionViewModel = koinViewModel<GetCompetitionViewModel>()
     NavHost(navController, startDestination = BottomBarData.Home.route) {
         composable(route = BottomBarData.Home.route) {
-            val state by getCompetitionViewModel.uiState.collectAsState()
-            CompetitionListPage(
-                state = state,
-                onTryAgain = { getCompetitionViewModel.fromUi(GetCompetitionEvent.TryAgain(it)) },
-                onClickToCreate = { getCompetitionViewModel.fromUi(GetCompetitionEvent.Create) },
-            )
+            MainScreen()
         }
         composable(route = BottomBarData.Overview.route) {
             OverviewScreen()
