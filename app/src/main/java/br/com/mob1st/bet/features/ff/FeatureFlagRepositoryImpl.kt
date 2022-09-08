3cde7bf433447f76bd79edfca3bd1c1756c72815
Pablo <pablo.baldez@getmayd.com>
2022-09-08 18:27:48 +0200

    feat: add remote config repository

---
+++ app/src/main/java/br/com/mob1st/bet/features/ff/FeatureFlagRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package br.com.mob1st.bet.features.ff
+
+import br.com.mob1st.bet.core.coroutines.DispatcherProvider
+import com.google.android.datatransport.runtime.dagger.Component.Factory
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.get
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+@Factory
+internal class FeatureFlagRepositoryImpl(
+    private val remoteConfig: FirebaseRemoteConfig,
+    private val provider: DispatcherProvider
+) : FeatureFlagRepository{
+
+    private val io get() = provider.io
+
+    override suspend fun sync(): Unit = withContext(io) {
+        runCatching {
+            remoteConfig.fetchAndActivate().await()
+        }.getOrElse {
+            throw InitRemoteConfigException(it)
+        }
+    }
+
+    override fun getBoolean(featureFlag: String): Boolean {
+        return remoteConfig[featureFlag].asBoolean()
+    }
+
+}
+
+class InitRemoteConfigException(cause: Throwable) : Exception("unable to init the remote config", cause)
