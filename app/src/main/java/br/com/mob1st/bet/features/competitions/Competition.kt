ac8cf41a878e611b4f2dc74dcc31ce406911623c
Pablo <pablo.baldez@getmayd.com>
2022-09-09 15:47:21 +0200

    build: remove kotlin serialization
    The firebase response is much more complex than I was expected, making the implementation of custom Serializers more complex then just mapping the values manually

--- app/src/main/java/br/com/mob1st/bet/features/competitions/Competition.kt
+++ app/src/main/java/br/com/mob1st/bet/features/competitions/Competition.kt
@@ -1,18 +1,12 @@
 package br.com.mob1st.bet.features.competitions
 
-import br.com.mob1st.bet.core.serialization.DateSerializer
-import kotlinx.serialization.Serializable
 import java.util.Date
 
-@Serializable
 data class Competition(
     val id: String,
     val code: String,
     val name: Map<String, String>,
-    val season: Int,
-    @Serializable(DateSerializer::class)
     val startAt: Date,
-    @Serializable(DateSerializer::class)
     val endAt: Date? = null,
     val type: CompetitionType
 )
