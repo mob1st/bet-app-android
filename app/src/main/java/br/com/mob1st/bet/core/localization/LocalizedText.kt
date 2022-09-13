960ae9f1a3fe36c414a7c98419a576580dfc2b05
Pablo <pablo.baldez@getmayd.com>
2022-09-13 19:56:47 +0200

    feat: helpful activity extensions

--- app/src/main/java/br/com/mob1st/bet/core/localization/LocalizedText.kt
+++ app/src/main/java/br/com/mob1st/bet/core/localization/LocalizedText.kt
@@ -1,8 +1,16 @@
 package br.com.mob1st.bet.core.localization
 
+import android.content.Context
+import androidx.core.os.ConfigurationCompat
+
 /**
  * A key-value group of words, where the key is the language and the value is the localized text
  */
 typealias LocalizedText = Map<String, String>
 
 val LocalizedText.default: String get() = getOrDefault("en-us", checkNotNull(get(keys.first())))
+
+fun Context.getText(text: LocalizedText): String {
+    val locales = ConfigurationCompat.getLocales(resources.configuration)
+    return text.getOrDefault(locales.toLanguageTags(), text.default)
+}
