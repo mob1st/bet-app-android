e929d243653beea5f74d76cfd39976cede6df9c0
Pablo <pablo.baldez@getmayd.com>
2022-09-14 14:56:01 +0200

    refact: change classes names

--- app/src/main/java/br/com/mob1st/bet/features/competitions/domain/Contest.kt
+++ app/src/main/java/br/com/mob1st/bet/features/competitions/domain/Contest.kt
@@ -8,25 +8,9 @@ import br.com.mob1st.bet.core.utils.objects.Duo
 sealed interface Contest
 
 /**
- * When two contenders face each other in a 1x1 confrontation, we have a Duel
- */
-interface Duel<T> {
-    val contender1: Bet<T>
-    val contender2: Bet<T>
-    val draw: Bet<Nothing>
-}
-
-/**
- * When many possibilities can happen during a confrontation,
- */
-interface MultiChoice<T> {
-    val contenders: List<Bet<T>>
-}
-
-/**
  * A match between two teams
  */
-data class FootballMatch(
+data class MatchWinner(
     override val contender1: Bet<Team>,
     override val contender2: Bet<Team>,
     override val draw: Bet<Nothing>
---
+++ app/src/main/java/br/com/mob1st/bet/features/competitions/domain/ContestSelector.kt
@@ -0,0 +1,40 @@
+package br.com.mob1st.bet.features.competitions.domain
+
+
+interface BetSelector<T> {
+    fun select(index: T): Bet<*>
+}
+
+/**
+ * When two contenders face each other in a 1x1 confrontation, we have a Duel
+ */
+interface Duel<T> : BetSelector<Duel.Selection>{
+    val contender1: Bet<T>
+    val contender2: Bet<T>
+    val draw: Bet<Nothing>
+
+    enum class Selection {
+        CONTENDER_1,
+        CONTENDER_2,
+        DRAW
+    }
+
+    override fun select(index: Selection): Bet<*> {
+        return when(index) {
+            Selection.CONTENDER_1 -> contender1
+            Selection.CONTENDER_2 -> contender2
+            Selection.DRAW -> draw
+        }
+    }
+}
+
+/**
+ * When many possibilities can happen during a confrontation,
+ */
+interface MultiChoice<T> : BetSelector<Int> {
+    val contenders: List<Bet<T>>
+
+    override fun select(index: Int): Bet<*> {
+        return contenders[index]
+    }
+}
