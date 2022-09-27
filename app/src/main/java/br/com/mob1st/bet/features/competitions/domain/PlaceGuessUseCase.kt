06eac1ecf6ffb37a1b04f3c9c5f3bf72318b7075
Pablo <pablo.baldez@getmayd.com>
2022-09-27 13:36:16 +0200

    refact: remove non used import

--- app/src/main/java/br/com/mob1st/bet/features/competitions/domain/PlaceGuessUseCase.kt
+++ app/src/main/java/br/com/mob1st/bet/features/competitions/domain/PlaceGuessUseCase.kt
@@ -4,7 +4,6 @@ import br.com.mob1st.bet.core.analytics.AnalyticsTool
 import br.com.mob1st.bet.core.coroutines.AppScopeProvider
 import br.com.mob1st.bet.core.logs.Debuggable
 import br.com.mob1st.bet.core.logs.Logger
-import br.com.mob1st.bet.features.profile.domain.AuthStatus
 import br.com.mob1st.bet.features.profile.domain.LoggedOut
 import br.com.mob1st.bet.features.profile.domain.User
 import br.com.mob1st.bet.features.profile.domain.UserRepository
