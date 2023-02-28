b7cf9fa38a860732111e926b86df1cb41583d587
pablobaldez <pablo.silva@traderepublic.com>
2023-02-28 14:07:29 +0100

    fail

--- app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageState.kt
+++ app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageState.kt
@@ -3,7 +3,6 @@ package br.com.mob1st.bet.core.ui.ds.organisms
 import br.com.mob1st.bet.R
 import br.com.mob1st.bet.core.tooling.androidx.TextData
 import br.com.mob1st.bet.core.ui.ds.molecule.ButtonState
-import kotlinx.collections.*
 
 sealed interface PageState {
 
--- app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageStateCrossfade.kt
+++ app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageStateCrossfade.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.*
+import androidx.compose.ui.Modifier
 
 @Composable
 fun <T> PageStateCrossfade(
