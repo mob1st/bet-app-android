30a94ea33209070282089ba0a11027997fd6eb0b
pablobaldez <pablo.silva@traderepublic.com>
2023-02-28 13:54:09 +0100

    should fail

--- app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageState.kt
+++ app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageState.kt
@@ -4,25 +4,24 @@ import br.com.mob1st.bet.R
 import br.com.mob1st.bet.core.tooling.androidx.TextData
 import br.com.mob1st.bet.core.ui.ds.molecule.ButtonState
 import br.com.mob1st.bet.core.ui.ds.molecule.SnackState
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.*
 
-sealed interface PageState<T> {
+sealed interface PageState {
 
-    data class Empty<T>(val loading: Boolean = true) : PageState<T>
+    data class Empty(val loading: Boolean = true) : PageState
 
-    data class Helper<T>(
+    data class Helper(
         val title: TextData,
         val description: TextData,
         val cta: ButtonState.Primary?
-    ) : PageState<T> {
+    ) : PageState {
 
         fun loading(loading: Boolean) = copy(
             cta = cta?.copy(loading = loading)
         )
 
         companion object {
-            fun <T> generalFailure(cta: ButtonState.Primary? = null) = Helper<T>(
+            fun generalFailure(cta: ButtonState.Primary? = null) = Helper(
                 title = TextData(R.string.general_message_error_title),
                 description = TextData(R.string.general_message_error_description),
                 cta = cta
@@ -32,14 +31,5 @@ sealed interface PageState<T> {
 
     data class Main<T>(
         val data: T,
-        val snacks: PersistentList<SnackState<*>> = persistentListOf(),
-        val refreshing: Boolean = false
-    ) : PageState<T> {
-
-        fun offer(snack: SnackState<*>) = copy(snacks = snacks.add(snack))
-
-        fun poll() = copy(snacks = snacks.removeAt(0))
-
-        fun peek(): SnackState<*>? = snacks.getOrNull(0)
-    }
+    ) : PageState
 }
