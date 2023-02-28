e3988d68180947fedcd60d6ba6435729a12f3d04
pablobaldez <pablo.silva@traderepublic.com>
2023-02-28 13:58:06 +0100

    try fail

--- app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageStateCrossfade.kt
+++ app/src/main/java/br/com/mob1st/bet/core/ui/ds/organisms/PageStateCrossfade.kt
@@ -7,14 +7,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.*
 
 @Composable
 fun <T> PageStateCrossfade(
     modifier: Modifier = Modifier,
-    pageState: PageState<T>,
-    empty: @Composable (PageState.Empty<T>) -> Unit,
-    helper: @Composable (PageState.Helper<T>) -> Unit,
+    pageState: PageState,
+    empty: @Composable (PageState.Empty) -> Unit,
+    helper: @Composable (PageState.Helper) -> Unit,
     main: @Composable (PageState.Main<T>) -> Unit
 ) {
     val switcher = remember(pageState) {
@@ -24,24 +24,24 @@ fun <T> PageStateCrossfade(
         when (val ps = target.pageState) {
             is PageState.Empty -> empty(ps)
             is PageState.Helper -> helper(ps)
-            is PageState.Main -> main(ps)
+            is PageState.Main<*> -> main(ps as PageState.Main<T>)
         }
     }
 }
 
 @Stable
-class PageSwithcer<T>(pageState: PageState<T>) {
+class PageSwithcer(pageState: PageState) {
 
     val state: State = when (pageState) {
         is PageState.Empty -> State.Empty
         is PageState.Helper -> State.Helper
-        is PageState.Main -> State.Main
+        is PageState.Main<*> -> State.Main
     }
 
     var pageState by mutableStateOf(pageState)
 
     override fun equals(other: Any?): Boolean {
-        return (this === other) || (other is PageSwithcer<*> && state == other.state)
+        return (this === other) || (other is PageSwithcer && state == other.state)
     }
 
     override fun hashCode(): Int {
