94fd12f989f9d65a97072dec0a43c1a1fdf7b648
pablobaldez <pablo.silva@traderepublic.com>
2023-03-10 21:26:57 +0100

    unit test two fields in consumable function

--- morpheus/processor/src/test/java/br/com/mob1st/processor/ConsumeExtensionFunctionalityTest.kt
+++ morpheus/processor/src/test/java/br/com/mob1st/processor/ConsumeExtensionFunctionalityTest.kt
@@ -11,33 +11,40 @@ import java.lang.reflect.Modifier
 
 class ConsumeExtensionFunctionalityTest : FunSpec({
 
-    test("consume method is calling the proper field") {
-        val result = compilation(
-            SourceFile.kotlin(
-                "Sample.kt",
-                """
-                
+    val compilation = compilation(
+        SourceFile.kotlin(
+            "Sample.kt",
+            """               
                 import br.com.mob1st.morpheus.annotation.ConsumableEffect
                 import br.com.mob1st.morpheus.annotation.Morpheus
                 @Morpheus
                 data class Sample(
                     @ConsumableEffect
-                    val field: String? = "value"
+                    val field1: String? = "value1",
+                    @ConsumableEffect
+                    val field2: String? = "value2"
                 )                    
-                """.trimIndent()
-            )
-        ).compile()
+            """.trimIndent()
+        )
+    )
+    test("consume method is calling the proper field") {
+        val result = compilation.compile()
         result.shouldExitOk()
         val extensionClass = result.classLoader.loadClass("SampleEffectKeyKt")
-
         val obj = result.classLoader.createInstance("Sample")
-
-        val consumeExtension = extensionClass.methods.first {
-            it.name == "consumeField" && Modifier.isStatic(it.modifiers)
-        }
-
-        @Suppress("UNCHECKED_CAST")
-        val consumable = consumeExtension.invoke(obj, obj) as Consumable<*, String>
-        consumable.effect shouldBe "value"
+        val field1Return = extensionClass.callConsumeFunction(obj, "Field1")
+        val field2Return = extensionClass.callConsumeFunction(obj, "Field2")
+        field1Return shouldBe "value1"
+        field2Return shouldBe "value2"
     }
 })
+
+private fun Class<*>.callConsumeFunction(obj: Any, fieldName: String): String {
+    val consumeExtension = methods.first {
+        it.name == "consume$fieldName" && Modifier.isStatic(it.modifiers)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    val consumable = consumeExtension.invoke(obj, obj) as Consumable<*, String>
+    return consumable.effect
+}
