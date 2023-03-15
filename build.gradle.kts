3a6c803b237ac353d8d8acd6afbb8ead5dcd49be
pablobaldez <pablo.silva@traderepublic.com>
2023-03-15 22:58:32 +0100

    try new jvm setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,6 +65,7 @@ subprojects {
 
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
         kotlinOptions.apply {
+            jvmTarget = "1.8"
             freeCompilerArgs = listOf("-Xcontext-receivers")
         }
     }
