f98c5b057b7855136fa2e76ba2895496e846e25d
pablobaldez <pablo.silva@traderepublic.com>
2023-03-22 19:36:15 +0100

    create onboarding module

---
+++ features/onboarding/impl/build.gradle.kts
@@ -0,0 +1,77 @@
+@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.google.ksp)
+    id("kotlin-parcelize")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "br.com.mob1st.bet.features.onboarding.impl"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+}
+
+dependencies {
+    // bundles
+    implementation(libs.bundles.accompanist)
+    implementation(libs.bundles.android)
+    implementation(libs.bundles.compose)
+    implementation(libs.bundles.firebase)
+    implementation(libs.bundles.koin)
+    implementation(libs.bundles.lifecycle)
+    implementation(libs.bundles.serialization)
+
+    // standalone
+    implementation(libs.coil)
+    implementation(libs.kotlin.collections)
+    implementation(libs.timber)
+
+    implementation(projects.morpheus.annotation)
+    debugImplementation(libs.compose.manifest)
+    debugImplementation(libs.compose.tooling)
+    debugImplementation(libs.leakcanary.debug)
+
+    ksp(libs.koin.compiler)
+    ksp(projects.morpheus.processor)
+
+    dokkaPlugin(libs.plugin.dokka.android)
+
+    testImplementation(libs.kotest)
+    testImplementation(libs.koin.test)
+
+    androidTestImplementation(libs.bundles.android.test)
+    androidTestImplementation(libs.compose.test)
+    androidTestImplementation(libs.leakcanary.test)
+}
