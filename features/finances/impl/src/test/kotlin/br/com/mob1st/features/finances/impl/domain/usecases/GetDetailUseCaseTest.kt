ae7f45779de1c297c33285367f1d343808c939f6
pablobaldez <pablo.silva@traderepublic.com>
2024-08-19 23:35:09 +0200

    fix get detail usecase

--- features/finances/impl/src/test/kotlin/br/com/mob1st/features/finances/impl/domain/usecases/GetDetailUseCaseTest.kt
+++ features/finances/impl/src/test/kotlin/br/com/mob1st/features/finances/impl/domain/usecases/GetDetailUseCaseTest.kt
@@ -1,6 +1,8 @@
 package br.com.mob1st.features.finances.impl.domain.usecases
 
+import br.com.mob1st.features.finances.impl.domain.entities.CalculatorPreferences
 import br.com.mob1st.features.finances.impl.domain.entities.Category
+import br.com.mob1st.features.finances.impl.domain.entities.CategoryDetail
 import br.com.mob1st.features.finances.impl.domain.entities.GetCategoryIntent
 import br.com.mob1st.features.finances.impl.domain.fixtures.asset
 import br.com.mob1st.features.finances.impl.domain.fixtures.category
@@ -44,7 +46,9 @@ class GetDetailUseCaseTest {
     @Test
     fun `GIVEN a list of assets WHEN get detail with create intention THEN assert the first asset is used`() = runTest {
         val assets = Arb.asset().chunked(2..10).next()
+        val calculatorPreferences = Arb.bind<CalculatorPreferences>().next()
         every { assetRepository.getByTag(any()) } returns flowOf(assets)
+        every { calculatorPreferencesRepository.get() } returns flowOf(calculatorPreferences)
         val intent = Arb.bind<GetCategoryIntent.Create>().next()
         val actual = useCase[intent].first()
         val asset = assets.first()
@@ -55,25 +59,36 @@ class GetDetailUseCaseTest {
     fun `GIVEN a list of assets WHEN get detail with create intention THEN assert default properties`() = runTest {
         val intent = Arb.bind<GetCategoryIntent.Create>().next()
         val assets = Arb.asset().chunked(1..3).next()
+        val calculatorPreferences = Arb.bind<CalculatorPreferences>().next()
         every { assetRepository.getByTag(intent.name) } returns flowOf(assets)
-        val expected = Category(
-            name = intent.name,
-            image = assets.first().uri,
-            isExpense = intent.defaultValues.isExpense,
-            recurrences = typeRecurrenceToRecurrences.getLeftValue(intent.defaultValues.recurrenceType),
-            isSuggested = false,
+        every { calculatorPreferencesRepository.get() } returns flowOf(calculatorPreferences)
+        val expected = CategoryDetail(
+            category = Category(
+                name = intent.name,
+                image = assets.first().uri,
+                isExpense = intent.defaultValues.isExpense,
+                recurrences = typeRecurrenceToRecurrences.getLeftValue(intent.defaultValues.recurrenceType),
+                isSuggested = false,
+            ),
+            preferences = calculatorPreferences,
         )
         val actual = useCase[intent].first()
-        assertEquals(expected, actual.category)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun `GIVEN a category WHEN get detail with edit intention THEN assert category is fetched by id`() = runTest {
         val intent = Arb.bind<GetCategoryIntent.Edit>().next()
+        val calculatorPreferences = Arb.bind<CalculatorPreferences>().next()
         val category = Arb.category().map { it.copy(id = intent.id) }.next()
         every { categoryRepository.getById(intent.id) } returns flowOf(category)
+        every { calculatorPreferencesRepository.get() } returns flowOf(calculatorPreferences)
         val actual = useCase[intent].first()
-        assertEquals(category, actual.category)
+        val expected = CategoryDetail(
+            category = category,
+            preferences = calculatorPreferences,
+        )
+        assertEquals(expected, actual)
         verify(exactly = 0) {
             assetRepository.getByTag(any())
         }
